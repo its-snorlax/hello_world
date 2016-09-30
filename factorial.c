7d99277b23d6510de9e48aeca345272974d996b3
minijainsahab <p.jain3322@gmail.com>
2016-09-30 18:17:14 +0530

    remove garbage before use fact

--- factorial.c
+++ factorial.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 void main(int argc, char const *argv[]) {
 	int input = atoi(argv[1]);
-	int fact , count;
+	int fact = 1, count;
 	for (count = 1; count <= input; count++){
 		fact = fact * count;
 	}
